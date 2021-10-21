0e5773ea948cbc399e76ef0c0126dfe21e102665
Oleg Stepanov <olezhka106s@gmail.com>
2021-10-21 21:48:44 +0300

    Initial Commit

---
+++ Jokes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jokes
+//
+//  Created by Oleg Stepanov on 21.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
